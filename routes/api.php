bbaf213bbc64d2bd463e11b1129eb216f963a5a8
JihanCahya <jihan69cahya@gmail.com>
2025-09-15 13:39:01 +0700

    fix route api

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->middleware('guest');
 Route::post('/register', [AuthController::class, 'register'])->middleware('guest');
-Route::get('/data-dashboard', [TransaksiController::class, 'dataDashboard']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/data-dashboard', [TransaksiController::class, 'dataDashboard']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/data-transaksi', [TransaksiController::class, 'data']);
 });
